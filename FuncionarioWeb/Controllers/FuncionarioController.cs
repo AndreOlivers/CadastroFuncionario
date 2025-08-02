5499eed8e6e2b0b65754fcd290c5c5e5c06528f0
AndreOlivers <andre.oliveira@imagemseguranca.com.br>
2025-08-02 13:24:20 -0300

    criacao controller

---
+++ FuncionarioWeb/Controllers/FuncionarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuncionarioWeb.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public FuncionarioController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
