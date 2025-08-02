547e0fd3b18ccc34372a924d959648fc9f1647b0
AndreOlivers <andre.oliveira@imagemseguranca.com.br>
2025-08-02 13:59:42 -0300

    ajuste testes unitários

--- FuncionarioTests/FuncionarioTestes/FuncionarioTeste.cs
+++ FuncionarioTests/FuncionarioTestes/FuncionarioTeste.cs
@@ -8,7 +8,6 @@ namespace SeuProjeto.Tests
         public void Deve_CriarFuncionario_ComDadosCorretos()
         {
             // Arrange
-            //int id = 1;
             string nome = "João";
             int idade = 30;
             double salario = 2500.50;
@@ -17,22 +16,21 @@ namespace SeuProjeto.Tests
             var funcionario = new Funcionario(nome, idade, salario);
 
             // Assert
-            //Assert.Equal(id, funcionario.Id);
             Assert.Equal(nome, funcionario.Nome);
             Assert.Equal(idade, funcionario.Idade);
             Assert.Equal(salario, funcionario.Salario);
         }
 
         [Theory]
-        [InlineData(1, "Ana", 28, 3000.00)]
-        [InlineData(2, "Carlos", 40, 4500.75)]
-        public void Deve_CriarFuncionario_UsandoDadosVariados(int id, string nome, int idade, double salario)
+        [InlineData("Ana", 28, 3000.00)]
+        [InlineData("Carlos", 40, 4500.75)]
+        public void Deve_CriarFuncionario_UsandoDadosVariados(
+            string nome, int idade, double salario)
         {
             // Act
             var funcionario = new Funcionario(nome, idade, salario);
 
             // Assert
-            //Assert.Equal(id, funcionario.Id);
             Assert.Equal(nome, funcionario.Nome);
             Assert.Equal(idade, funcionario.Idade);
             Assert.Equal(salario, funcionario.Salario);
